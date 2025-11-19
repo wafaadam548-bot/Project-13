7e4a01434d429f57c472be72cc979180e69e7c91
Wafa Adam <wafaadam548@gmail.com>
2025-11-19 06:19:37 -0500

    print the welcome to the bmi app

--- BMI.cpp
+++ BMI.cpp
@@ -1,4 +1,8 @@
 # include <iostream>
 int main(){
+    std::cout<<"________________________________________";
+    std::cout<<"Welcome to the BMI app \n";
+    std::cout<<"Let us Calcolate your BMI";
+    
     
 }
