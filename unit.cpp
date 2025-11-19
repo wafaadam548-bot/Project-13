b79b499579a0f575852998796536e3cfa33856d4
Wafa Adam <wafaadam548@gmail.com>
2025-11-19 06:14:31 -0500

    add the unit

---
+++ unit.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+int main(){
+    char unit1[]="Height";
+    char unit2[]="Width";
+    char unit3[]="Time";
+    char unit4[]="Temperature";
+    std::cout<<"___________________________________________\n";
+    std::cout<<"Welcome to our unit Converter app\n";
+    std::cout<<"Choose what you want to convert\n";
+    std::cout<<unit1<<"\n";
+    std::cout<<unit2<<"\n";
+    std::cout<<unit3<<"\n";
+    std::cout<<unit4<<"\n";
+    
+}
