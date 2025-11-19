ca298a3b4f84478284ab916ea7083f674bf3cf63
Wafa Adam <wafaadam548@gmail.com>
2025-11-19 05:30:42 -0500

    add the contory names

--- Coin.cpp
+++ Coin.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 int main(){
+    char contory1[]="USD";           
+    char contory2[]="EUR";
+    char contory3[]="SAR";
+    char contory4[]="AED";
+    char contory5[]="GBP";
+    char contory6[]="JPY";
+    char contory7[]="CNY";
+    char contory8[]="TRY";
+    char contory9[]="CAD";
+    char contory10[]="AUD";
+    char contory11[]="INR";
+    std:: cout<<"__________________________________________________________\n";
+    std::cout<<"Welcome to Currency converter  =) \n";
+    std::cout<<"____________________________________________________________\n";
+    std::cout<<contory1<<"\n";
+    std::cout<<contory2<<"\n";
+    std::cout<<contory3<<"\n";
+    std::cout<<contory4<<"\n";
+    std::cout<<contory5<<"\n";
+    std::cout<<contory6<<"\n";
+    std::cout<<contory7<<"\n";
+    std::cout<<contory8<<"\n";
+    std::cout<<contory9<<"\n";
+    std::cout<<contory10<<"\n";
+    std::cout<<contory11<<"\n";
     
+
+
 }
