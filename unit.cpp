58819ca4eacea18e1286aa11c73b5211c30ec5b2
Wafa Adam <wafaadam548@gmail.com>
2025-11-29 07:34:16 -0500

    try to make array insteead of if statment

--- unit.cpp
+++ unit.cpp
@@ -7,9 +7,14 @@ int main(){
     std::cout<<"___________________________________________\n";
     std::cout<<"Welcome to our unit Converter app\n";
     std::cout<<"Choose what you want to convert\n";
-    std::cout<<unit1<<"\n";
-    std::cout<<unit2<<"\n";
-    std::cout<<unit3<<"\n";
-    std::cout<<unit4<<"\n";
+    std::cout<<"1/"<<unit1<<"\n";
+    std::cout<<"2/"<<unit2<<"\n";
+    std::cout<<"3/"<<unit3<<"\n";
+    std::cout<<"4/"<<unit4<<"\n";
+    int choice;
+    std::cout<<"Enter the number of the unit :";
+    std::cin>>choice;
+
+
     
 }
